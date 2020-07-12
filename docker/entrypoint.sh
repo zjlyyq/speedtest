063a6cc371eae1d5774eef3e30e596db575bdb85
Zhang Sen <senorsen.zhang@gmail.com>
2020-07-12 19:47:17 +0800

    Fix #334 wrong port config generated (#335)
    * Fix #334 wrong port config generated
    
    * Update docker/entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -53,7 +53,8 @@ fi
 
 # Allow selection of Apache port for network_mode: host
 if [ "$WEBPORT" != "80" ]; then
-  sed -i "s/80/$WEBPORT/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
+  sed -i "s/^Listen 80\$/Listen $WEBPORT/g" /etc/apache2/ports.conf
+  sed -i "s/*:80>/*:$WEBPORT>/g" /etc/apache2/sites-available/000-default.conf 
 fi
 
 
